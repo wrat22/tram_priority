dd177d4deb003ec6c82c838143c83ade70802510
wrat2 <bartekwrat@gmail.com>
2024-02-23 19:19:52 +0100

    Changed names for variables to english

--- scripts/sip_effectiveness_analysis.py
+++ scripts/sip_effectiveness_analysis.py
@@ -3,22 +3,22 @@ import pandas as pd
 def sip_effectiveness(data, sip_det, det_before_cross):
     df = pd.DataFrame(data)
 
-    lista_niewykrytych = []
-    licznik_tramwajow_przed_skrzyzowaniem, licznik_wykrytych_przez_sip, licznik_ominietych_przez_sip = 0, 0, 0
+    undetected_list = []
+    count_trams_before_cross, count_sip_detected, count_sip_undetected = 0, 0, 0
 
     for index, row in df.iterrows():
         if row["Urzadzenie"] == sip_det and row["Stan"] == 1:
-            licznik_tramwajow_przed_skrzyzowaniem += 1
-            print(row["Czas"], licznik_tramwajow_przed_skrzyzowaniem)
+            count_trams_before_cross += 1
+            print(row["Czas"], count_trams_before_cross)
         elif row["Urzadzenie"] == det_before_cross and row["Stan"] == 1:
-            if licznik_tramwajow_przed_skrzyzowaniem > 0:
-                licznik_wykrytych_przez_sip += 1
-                licznik_tramwajow_przed_skrzyzowaniem -= 1
+            if count_trams_before_cross > 0:
+                count_sip_detected += 1
+                count_trams_before_cross -= 1
             else:
-                licznik_ominietych_przez_sip += 1
-                lista_niewykrytych.append(row["Czas"])
+                count_sip_undetected += 1
+                undetected_list.append(row["Czas"])
     
 
-    print(f"Liczba wykrytych: {licznik_wykrytych_przez_sip}")
-    print(f"Liczba niewykrytych: {licznik_ominietych_przez_sip}")
-    print(lista_niewykrytych)
+    print(f"Liczba wykrytych: {count_sip_detected}")
+    print(f"Liczba niewykrytych: {count_sip_undetected}")
+    print(undetected_list)
